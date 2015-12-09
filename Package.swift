2884b75adb23171039e733268fb05b93c4815de0
David Ask <david@formbound.com>
2015-12-09 18:39:35 +0100

    Updates

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Postgres",
     dependencies: [
-        .Package(url: "https://github.com/formbound/SwiftSQL.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/formbound/SwiftSQL.git", majorVersion: 0),
 	.Package(url: "https://github.com/formbound/libpq.git", majorVersion: 9)
     ]
 )
