fe4bb36e5dbb04f6e7a5f1176663902805dd8127
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 06:57:55 -0800

    fix 2015-12-18 toolchain SPM bug

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 import PackageDescription
-import Foundation
 
 #if os(OSX)
 	let libpqURL = "https://github.com/formbound/CLibpq-OSX.git"
@@ -10,7 +9,8 @@ import Foundation
 let package = Package(
     name: "PostgreSQL",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0),
-		.Package(url: libpqURL, majorVersion: 0)
+        .Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0, minor: 1),
+		.Package(url: libpqURL, majorVersion: 0, minor: 1),
+		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
     ]
 )
