cdb69e3f1dd138a5661022f2a1a7520f3316349b
antonmes <antonmes@users.noreply.github.com>
2016-04-20 02:11:08 +0300

    Remove -OSX dependency

--- Package.swift
+++ Package.swift
@@ -1,15 +1,9 @@
 import PackageDescription
 
-#if os(OSX)
-	let CLibpqURL = "https://github.com/Zewo/CLibpq-OSX.git"
-#else
-	let CLibpqURL = "https://github.com/Zewo/CLibpq.git"
-#endif
-
 let package = Package(
 	name: "PostgreSQL",
 	dependencies: [
-		.Package(url: CLibpqURL, majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/Zewo/CLibpq.git", majorVersion: 0, minor: 5),
 		.Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0, minor: 5),
 	]
 )
