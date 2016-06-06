c2ce432ad88e380e7887dd5931b0d62c07f08d28
David Ask <david@formbound.com>
2016-06-06 17:08:42 +0200

    Develop for

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
 	name: "PostgreSQL",
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/CLibpq.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0, minor: 7),
+		.Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0),
 	]
 )
