bb579c183f0e584f77009a55b4770cbb6fb8355a
Jörn Röder <kontakt@joernroeder.de>
2016-09-27 21:47:09 +0200

    Connection refactor

--- Sources/PostgreSQL/Connection.swift
+++ Sources/PostgreSQL/Connection.swift
@@ -1,14 +1,16 @@
 @_exported import SQL
 import CLibpq
+import Core
+
+public struct ConnectionError: Error, CustomStringConvertible {
+	public let description: String
+}
 
 public final class Connection: ConnectionProtocol {
     public typealias QueryRenderer = PostgreSQL.QueryRenderer
 
-    public struct Error: ErrorProtocol, CustomStringConvertible {
-        public let description: String
-    }
-
-    public struct ConnectionInfo: ConnectionInfoProtocol {
+	public struct ConnectionInfo: ConnectionInfoProtocol {
+		
         public var host: String
         public var port: Int
         public var databaseName: String
@@ -16,18 +18,27 @@ public final class Connection: ConnectionProtocol {
         public var password: String?
         public var options: String?
         public var tty: String?
+		
+		public init?(uri: URL) {
+			do {
+				try self.init(uri)
+			} catch {
+				return nil
+			}
+		}
+        public init(_ uri: URL) throws {
 
-        public init(_ uri: URI) throws {
-
-            guard let host = uri.host, port = uri.port, databaseName = uri.path?.trim(["/"]) else {
-                throw Error(description: "Failed to extract host, port, database name from URI")
+			let databaseName = uri.path.trim(["/"])
+			
+            guard let host = uri.host, let port = uri.port else {
+                throw ConnectionError(description: "Failed to extract host, port, database name from URI")
             }
 
             self.host = host
             self.port = port
             self.databaseName = databaseName
-            self.username = uri.userInfo?.username
-            self.password = uri.userInfo?.password
+            self.username = uri.user
+            self.password = uri.password
 
         }
         public init(host: String, port: Int = 5432, databaseName: String, password: String? = nil, options: String? = nil, tty: String? = nil) {
@@ -38,6 +49,7 @@ public final class Connection: ConnectionProtocol {
             self.options = options
             self.tty = tty
         }
+		
     }
 
 
@@ -125,11 +137,11 @@ public final class Connection: ConnectionProtocol {
     }
 
     public var mostRecentError: Error? {
-        guard let errorString = String(validatingUTF8: PQerrorMessage(connection)) where !errorString.isEmpty else {
+        guard let errorString = String(validatingUTF8: PQerrorMessage(connection)), !errorString.isEmpty else {
             return nil
         }
 
-        return Error(description: errorString)
+        return ConnectionError(description: errorString)
     }
 
     public func close() {
@@ -138,15 +150,15 @@ public final class Connection: ConnectionProtocol {
     }
 
     public func createSavePointNamed(_ name: String) throws {
-        try execute("SAVEPOINT \(name)")
+		try execute("SAVEPOINT \(name)", parameters: nil)
     }
 
     public func rollbackToSavePointNamed(_ name: String) throws {
-        try execute("ROLLBACK TO SAVEPOINT \(name)")
+        try execute("ROLLBACK TO SAVEPOINT \(name)", parameters: nil)
     }
 
     public func releaseSavePointNamed(_ name: String) throws {
-        try execute("RELEASE SAVEPOINT \(name)")
+        try execute("RELEASE SAVEPOINT \(name)", parameters: nil)
     }
 
     public func execute(_ statement: String, parameters: [Value?]?) throws -> Result {
@@ -159,7 +171,7 @@ public final class Connection: ConnectionProtocol {
 
         guard let parameters = parameters else {
             guard let resultPointer = PQexec(connection, statement) else {
-                throw mostRecentError ?? Error(description: "Empty result")
+                throw mostRecentError ?? ConnectionError(description: "Empty result")
             }
 
             return try Result(resultPointer)
@@ -184,7 +196,7 @@ public final class Connection: ConnectionProtocol {
             }
         }
 
-
+		
         guard let result:OpaquePointer = PQexecParams(
             self.connection,
             statement,
@@ -195,7 +207,7 @@ public final class Connection: ConnectionProtocol {
             nil,
             0
             ) else {
-                throw mostRecentError ?? Error(description: "Empty result")
+                throw mostRecentError ?? ConnectionError(description: "Empty result")
         }
 
         return try Result(result)
